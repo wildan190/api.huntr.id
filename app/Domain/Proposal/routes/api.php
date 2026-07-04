cbca4a7a241565657a460861af3814765ae5254b
Wildan Belfiore <mikewildan@outlook.com>
2026-07-04 20:11:52 +0700

    update

--- app/Domain/Proposal/routes/api.php
+++ app/Domain/Proposal/routes/api.php
@@ -7,10 +7,14 @@ Route::prefix('api/proposals')->middleware(['api', 'auth:api'])->group(function 
     Route::get('', [ProposalController::class, 'index']);
     Route::post('', [ProposalController::class, 'store']);
     Route::get('my-rank', [ProposalController::class, 'vendorRankings']);
-    Route::get('manager/awaiting-approval', [ProposalController::class, 'awaitingApproval']);
     Route::get('{proposal}', [ProposalController::class, 'show']);
-    Route::post('{proposal}/award', [ProposalController::class, 'awardWinner']);
-    Route::post('{proposal}/approve', [ProposalController::class, 'approveWinner']);
+    
+    // Manager-only routes for proposal approval and awarding
+    Route::middleware('manager.only')->group(function () {
+        Route::get('manager/awaiting-approval', [ProposalController::class, 'awaitingApproval']);
+        Route::post('{proposal}/award', [ProposalController::class, 'awardWinner']);
+        Route::post('{proposal}/approve', [ProposalController::class, 'approveWinner']);
+    });
 });
 
 Route::prefix('api/rfqs')->middleware(['api', 'auth:api'])->group(function () {
